ae1cd2d968a72002134c59f03f1687c1bf5a8ac5
Makrand Gupta <makrand1996@gmail.com>
2018-09-10 16:30:15 -0400

    auto-populate all contact info and use flexbox for display

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { contact } from '../../data.json';
+import './Contact.css';
 
 export default class Contact extends Component {
   constructor(props) {
@@ -13,7 +14,17 @@ export default class Contact extends Component {
     return (
       <div className="component-container">
         <div className="component-title">Contact</div>
-        <div className="component-content">Email: {this.state.contact.email}</div>
+        <div className="component-content flex horizontal wrap space-evenly">
+
+          {
+            this.state.contact.map((contact, i) => (
+              <div className="contact-container" key={i}>
+                <div className="contact-header">{contact.type}</div>
+                <div className="contact-data">{contact.value}</div>
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
