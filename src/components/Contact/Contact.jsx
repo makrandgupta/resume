21f9af2809692f0bbc32d949d955e3d5b8114919
Makrand Gupta <makrand1996@gmail.com>
2018-09-12 03:15:53 -0400

    inital setup for ReactModal

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 import { contact } from '../../data.json';
+import AddButton from '../Buttons/AddButton/AddButton';
+import CloseButton from '../Buttons/CloseButton/CloseButton';
 import './Contact.css';
+
 
 export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
       contact,
+      showModal: false,
     };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({
+      showModal: true,
+    });
+  }
+
+  handleCloseModal() {
+    this.setState({
+      showModal: false,
+    });
   }
 
   render() {
     return (
       <div className="component-container">
-        <div className="component-title">Contact</div>
+        <div className="component-title">
+          Contact
+          <AddButton onClick={this.handleOpenModal}/>
+        </div>
         <div className="component-content flex horizontal wrap space-evenly">
-
           {
             this.state.contact.map((contact, i) => (
               <div className="contact-container" key={i}>
@@ -25,6 +47,15 @@ export default class Contact extends Component {
             ))
           }
         </div>
+
+        <ReactModal
+          isOpen={this.state.showModal}
+          onRequestClose={this.handleCloseModal}
+        >
+          Type: [INPUT CONTACT TYPE] <br />
+          Value: [INPUT CONTACT VALUE]
+          <CloseButton onClick={this.handleCloseModal} />
+        </ReactModal>
       </div>
     );
   }
