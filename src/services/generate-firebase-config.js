4d167b872a4c2ec7be243dd3b1852181ea3fc5b9
Makrand Gupta <makrand1996@gmail.com>
2018-09-20 15:56:25 -0400

    add js script to generate firebase secrets file

---
+++ src/services/generate-firebase-config.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const config = {}
+config['apiKey'] = _.get(process, 'env.FIREBASE_API_KEY', '');
+config['authDomain'] = _.get(process, 'env.FIREBASE_AUTH_DOMAIN', '');
+config['databaseURL'] = _.get(process, 'env.FIREBASE_DATABSE_URL', '');
+
+fs.writeFileSync('./firebase-config.json', JSON.stringify(config));
