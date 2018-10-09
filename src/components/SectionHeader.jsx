2644619b0dcf557fa5d8fd62dde87e943e12b0b9
Makrand Gupta <makrand1996@gmail.com>
2018-10-09 00:57:25 -0400

    remove old code

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -19,29 +19,4 @@ const SectionHeader = ({ sectionName, openAddForm, closeAddForm}) => (
   </Menu>
 );
 
-// class SectionHeader extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   render() {
-//     return (
-//       <Menu borderless>
-//         <Menu.Item>
-//           <Header as="h2" textAlign="left">Contact</Header>
-//         </Menu.Item>
-
-//         <Menu.Item>
-//           <AddButton onClick={this.props.openAddForm} />
-//         </Menu.Item>
-
-//         <Menu.Item>
-//           <CloseButton onClick={this.props.closeAddForm} />
-//         </Menu.Item>
-//       </Menu>
-//     )
-//   }
-// }
-
 export default SectionHeader;
