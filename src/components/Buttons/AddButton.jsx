c289fcb0c0f8e84ceba322a734d07b1de62c8f79
Makrand Gupta <makrand1996@gmail.com>
2018-10-10 18:18:10 -0400

    change AddButton to a more low profile style

--- src/components/Buttons/AddButton.jsx
+++ src/components/Buttons/AddButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 
-const AddButton = ({ onClick }) => <Button onClick={onClick} circular icon="add"></Button>;
+const AddButton = ({ onClick }) => <Button basic onClick={onClick} circular icon="add"></Button>;
 
 export default AddButton;
