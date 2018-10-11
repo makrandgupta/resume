457f3917e9edb6440c00612e39d09aa2fc2b8ead
Makrand Gupta <makrand1996@gmail.com>
2018-10-11 13:18:11 -0400

    add action handlers to EditModal

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -1,19 +1,50 @@
+import _ from 'lodash';
 import React from 'react';
 import { Button, Modal } from 'semantic-ui-react';
+import ContactForm from './Contact/ContactForm';
+import ExperienceForm from './Experience/ExperienceForm';
+import SkillsForm from './Skills/SkillsForm';
+import EducationForm from './Education/EducationForm';
 
 class EditModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = { ...(_.get(this.props, 'data')) };
+  }
+
+  forms = {
+    contact: ContactForm,
+    experience: ExperienceForm,
+    skills: SkillsForm,
+    education: EducationForm,
+  }
+
+  handleSave = () => {
+    this.props.onSave(_.assign({}, this.props.data, _.pickBy(this.state)));
+    this.props.onClose();
+  }
+
+  handleDelete = () => {
+    this.props.onDelete(_.get(this.props, 'data.key'));
+    this.props.onClose();
+  }
+
+  handleFormChange = (event, data) => {
+    const src = _.isEmpty(data) ? event.target : data;
+    // Checkbox component uses 'checked' instead of 'value' to store the current state
+    src.value = _.get(src, 'type') === 'checkbox' ? src.checked : src.value;
+    this.setState({
+      [src.name]: src.value
+    });
   }
 
   render() {
+    const EditForm = this.forms[this.props.section];
     return (
       <Modal basic closeIcon open={this.props.open} onClose={this.props.onClose}>
-        <Modal.Header>{this.props.header}</Modal.Header>
+        <Modal.Header>Edit {this.props.section}</Modal.Header>
         <Modal.Content>
-          {this.props.children}
+          <EditForm data={this.props.data} onChange={this.handleFormChange} />
         </Modal.Content>
         <Modal.Actions>
           <Button basic inverted onClick={this.props.onClose}>
@@ -26,7 +57,7 @@ class EditModal extends React.Component {
             icon='delete'
             labelPosition='left'
             content="Delete"
-            onClick={this.props.onDelete}
+            onClick={this.handleDelete}
           />
           <Button
             positive
@@ -34,7 +65,7 @@ class EditModal extends React.Component {
             icon='save'
             labelPosition='right'
             content="Save"
-            onClick={this.props.onSave}
+            onClick={this.handleSave}
           />
         </Modal.Actions>
       </Modal>
