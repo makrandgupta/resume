86650477f4529c5f4471696936beb51e298e2ba5
Makrand Gupta <makrand1996@gmail.com>
2018-10-28 01:54:21 -0400

    rename login to sign in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,12 +3,8 @@ import FacebookButton from './Buttons/FacebookButton';
 import GithubButton from './Buttons/GithubButton';
 import GoogleButton from './Buttons/GoogleButton';
 
-
-
 const Login = ( { authenticate }) => (
   <nav>
-    <h2>Login</h2>
-
     <p>Sign in to manage your resume.</p>
 
     <GithubButton onClick={() => authenticate('Github')}/>
